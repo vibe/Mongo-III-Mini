ed2d80ee943a25988b43dc184cee855adc17b281
Christian Franco <christian.franco94@icloud.com>
2017-11-29 17:35:53 -0800

    post controller

--- api/controllers/postsController.js
+++ api/controllers/postsController.js
@@ -1,25 +1,49 @@
 const mongoose = require('mongoose');
-
-const Post = mongoose.model('Post');
-const Comment = mongoose.model('Comment');
+const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 const STATUS_USER_ERROR = 422;
 
 /* Fill in each of the below controller methods */
 const createPost = (req, res) => {
-
+  const { title, text } = req.body;
+  const newPost = new Post({ title, text });
+  newPost.save((error, createdPost) => {
+    return error ? res.status(STATUS_USER_ERROR).json({ error: error.message }) : res.json(createdPost);
+  });
 };
 
 const listPosts = (req, res) => {
-
+  Post.find({}).populate('comments', 'text').exec((error, posts) => {
+    return error ? res.status(500).json({ error: error.message }) : res.json(posts);
+  });
 };
 
 const findPost = (req, res) => {
-
+  const { id } = req.params;
+  
 };
 
 const addComment = (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
 
+  const newComment = new Comment({ _parent: id, text });
+  newComment.save((error, comment) => {
+    if (error) {
+      return res.status(STATUS_USER_ERROR).json({ error: error.message });
+    }
+    Post.findById(id, (error, post) => {
+      if (error) {
+        res.status(STATUS_USER_ERROR).json({ error: error.message });
+        return;
+      }
+      post.comments.push(comment);
+      post.save((err, p) => {
+        return error ? res.status(STATUS_USER_ERROR).json({ error: err.message }) : res.json({ success: true });
+      });
+    });
+  });
 };
 
 // In this function, we need to delete the comment document
